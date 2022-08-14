18fad83cb45d6759a6bc61f50e33d05e7178fc2a
Catherine Phung <101031836+cnpdeakin@users.noreply.github.com>
2022-08-14 15:36:33 +1000

    Update Task1.3C.ino

--- Module1/Task1.3C.ino
+++ Module1/Task1.3C.ino
@@ -1,8 +1,7 @@
 // C++ code
 
-int soilPin = A0;				// Soil Out pin
-int pirPin = 2;                 // PIR Out pin 
-int pirStat = 0;                // PIR status
+int pirPin1 = 2;                // PIR1 Out pin
+int pirPin2 = 3; 				// PIR2 Out pin
 int ledState = LOW;				// LED state
 
 
@@ -10,28 +9,30 @@ int ledState = LOW;				// LED state
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(pirPin, INPUT_PULLUP);
-  pinMode(soilPin, INPUT);
+  pinMode(pirPin1, INPUT_PULLUP);
+  pinMode(pirPin2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), toggleLED, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(3), toggleLED, CHANGE);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  //reads the soil pin and outputs the value to the serial every second
-  float soilReading = analogRead(soilPin);
-  Serial.println("Soil Reading: " + String(soilReading));
   delay(1000); // Wait for 1000 millisecond(s)
 }
 
-//Function will be toggled when pin 2 (pirPIN) changes state
+//Function will be toggled when motion is detected changes state
 void toggleLED()
 {
+  int pirStat1 = digitalRead(pirPin1);
+  int pirStat2 = digitalRead(pirPin2); 
+  
   ledState = !ledState;
-  pirStat = !pirStat;
+ 
   digitalWrite(LED_BUILTIN, ledState);
   
   //Print sensor and LED state to serial monitor
-  Serial.println("SENSOR INPUT: "+ String(pirStat) +
+  Serial.println("SENSOR 1 INPUT: "+ String(pirStat1) +
+                 " || SENSOR 2 INPUT: " + String(pirStat2) +
                " || LED STATE: " + String(ledState));
 }
